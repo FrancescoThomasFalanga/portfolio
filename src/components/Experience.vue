d0ad28e136e6b7ff9c85d34f15a3a67f55ec1654
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-07-16 15:34:54 +0200

    init other files

---
+++ src/components/Experience.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <div class="mine" id="5"></div>
+</template>
+
+<style lang="scss" scoped>
+.mine {
+  background-color: white;
+  min-height: calc(90vh - 40px);
+  width: 100%;
+  position: relative;
+  z-index: 2;
+  //   margin-top: 60px;
+  padding-top: 40px;
+}
+</style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <div class="mine" id="4"></div>
+</template>
+
+<style lang="scss" scoped>
+.mine {
+  background-color: #05000d;
+  min-height: calc(90vh - 40px);
+  width: 100%;
+  position: relative;
+  z-index: 2;
+  //   margin-top: 60px;
+  padding-top: 40px;
+}
+</style>
