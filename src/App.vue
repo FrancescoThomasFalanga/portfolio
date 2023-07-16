6c0b535823f73e5b3fb913d3df48557fe0787ffb
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-07-16 15:34:08 +0200

    add Files

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
 import Home from "./components/Home.vue";
 import AboutMe from "./components/AboutMe.vue";
 import Skills from "./components/Skills.vue";
+import Education from "./components/Education.vue";
+import Projects from "./components/Projects.vue";
+import Contact from "./components/Contact.vue";
 
 export default {
   data() {
@@ -18,6 +21,9 @@ export default {
     Home,
     AboutMe,
     Skills,
+    Education,
+    Projects,
+    Contact,
   },
 
   methods: {
@@ -44,6 +50,9 @@ export default {
     <Home></Home>
     <AboutMe></AboutMe>
     <Skills></Skills>
+    <Education></Education>
+    <Projects></Projects>
+    <Contact></Contact>
 
     <button @click="goUp()" class="go-up" v-show="!isHome">
       <i class="fa-solid fa-circle-up"></i>
